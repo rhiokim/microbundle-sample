60915d7156c14f4f332718b60638d22e9948dfa6
rhiokim <rhio.kim@gmail.com>
2018-02-27 00:15:16 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,2 @@
+var r=function(r,n){for(var o=[],e=arguments.length-2;e-- >0;)o[e]=arguments[e+2];return{tag:r,props:n,children:o}},n=function(){};n.prototype.render=function(){return r("div",{id:"app"},r("h1",null,"Hello, World!"))},module.exports=n;
+//# sourceMappingURL=index.js.map
